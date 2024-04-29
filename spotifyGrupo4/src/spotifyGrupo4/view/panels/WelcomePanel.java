2acc9d1991d006157a0eccfa0f8ea4cb3f2097d9
SasoriAka96 <nahivallejo46@gmail.com>
2024-04-29 20:59:16 +0200

    WelcomePanel Añadido

---
+++ spotifyGrupo4/src/spotifyGrupo4/view/panels/WelcomePanel.java
@@ -0,0 +1,23 @@
+package spotifyGrupo4.view.panels;
+
+import java.awt.Color;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+public class WelcomePanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+
+	public WelcomePanel(List<JPanel> panels) {
+
+		setBackground(new Color(159, 203, 217));
+		setBounds(100, 100, 1278, 719);
+		setLayout(null);
+	}
+
+}
