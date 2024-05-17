fb4baf81bc8c7534c3846a9cb563f6af6629eab4
in1dm3-v <in1dm3-v@HZ327397.elorrieta.int>
2024-05-17 19:08:19 +0200

    coments deletes

--- spotifyGrupo4/src/spotifyGrupo4/view/panels/MisFavoritos.java
+++ spotifyGrupo4/src/spotifyGrupo4/view/panels/MisFavoritos.java
@@ -37,40 +37,30 @@ public class MisFavoritos extends JPanel {
 	private JPopupMenu popup;
 	private JTable tableContent;
 
-
 	/**
 	 * Create the panel.
 	 */
 	public MisFavoritos(List<JPanel> panels) {
 
-
-
-		
 		setBounds(309, 0, 953, 618);
 		setBackground(new Color(102, 205, 170));
 		setLayout(null);
-		
-		//creating scrollpane
-		JScrollPane scrollPlaylists= new JScrollPane();
+
+		JScrollPane scrollPlaylists = new JScrollPane();
 		scrollPlaylists.setBounds(167, 50, 573, 150);
 		add(scrollPlaylists);
-		
-		
-		//creating model fro table of playlist
+
 		DefaultTableModel modelo = new DefaultTableModel();
 		modelo.addColumn("idPalylist");
 		modelo.addColumn("Title");
 
-		//create table
 		table = new JTable(modelo);
 		scrollPlaylists.setViewportView(table);
-		
-		//same stuff
-		JScrollPane scrollPlaylistsContent= new JScrollPane();
+
+		JScrollPane scrollPlaylistsContent = new JScrollPane();
 		scrollPlaylistsContent.setBounds(167, 200, 573, 200);
 		add(scrollPlaylistsContent);
-		
-		
+
 		DefaultTableModel modeloCanciones = new DefaultTableModel();
 		modeloCanciones.addColumn("Title");
 		modeloCanciones.addColumn("legnth");
@@ -79,210 +69,167 @@ public class MisFavoritos extends JPanel {
 		scrollPlaylistsContent.setViewportView(tableContent);
 
 		popup = new JPopupMenu();
-        JMenuItem menuDelete = new JMenuItem("Delete");
-        menuDelete.addActionListener( new ActionListener(){
-        	
-        	  public void actionPerformed(ActionEvent e)
-  	        {
-  	            JTable table;
+		JMenuItem menuDelete = new JMenuItem("Delete");
+		menuDelete.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				JTable table;
 				try {
-					Component c = (Component)e.getSource();
-					JPopupMenu popup = (JPopupMenu)c.getParent();
-					table = (JTable)popup.getInvoker();
+					Component c = (Component) e.getSource();
+					JPopupMenu popup = (JPopupMenu) c.getParent();
+					table = (JTable) popup.getInvoker();
 					int column = 0;
 					int row = table.getSelectedRow();
 					int value = Integer.parseInt((table.getModel().getValueAt(row, column)).toString());
 					new PlaylistController().erase(value);
 				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				} catch (Exception e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-//  	            System.out.println("Delete");
-//  	            System.out.println(table.getSelectedRow() + " : " + table.getSelectedColumn());
-  	        }
-        });
-        
-        popup.add( menuDelete );
-        JMenuItem menuCreateOne = new JMenuItem("Any other Option");
-        menuCreateOne.addActionListener( new ActionListener(){
-        	
-      	  public void actionPerformed(ActionEvent e)
-	        {
-	            Component c = (Component)e.getSource();
-	            JPopupMenu popup = (JPopupMenu)c.getParent();
-	            JTable table = (JTable)popup.getInvoker();
-//	            System.out.println("CREATEONE");
-//	            System.out.println(table.getSelectedRow() + " : " + table.getSelectedColumn());
-	            int column = 0;
-	            int row = table.getSelectedRow();
-	            String value = table.getModel().getValueAt(row, column).toString();
-	            System.out.println(value);
-	        }
-      });
-        
-        popup.add( menuCreateOne );
-        JMenuItem menuViewContent = new JMenuItem("ViewContent");
-        menuViewContent.addActionListener(new ActionListener(){
-        	
-      	  public void actionPerformed(ActionEvent e)
-	        {
-	            JTable table;
+			}
+		});
+
+		popup.add(menuDelete);
+		JMenuItem menuCreateOne = new JMenuItem("Any other Option");
+		menuCreateOne.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				Component c = (Component) e.getSource();
+				JPopupMenu popup = (JPopupMenu) c.getParent();
+				JTable table = (JTable) popup.getInvoker();
+				int column = 0;
+				int row = table.getSelectedRow();
+				String value = table.getModel().getValueAt(row, column).toString();
+				System.out.println(value);
+			}
+		});
+
+		popup.add(menuCreateOne);
+		JMenuItem menuViewContent = new JMenuItem("ViewContent");
+		menuViewContent.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				JTable table;
 				try {
-					
-					Component c = (Component)e.getSource();
-					JPopupMenu popup = (JPopupMenu)c.getParent();
-					table = (JTable)popup.getInvoker();
+
+					Component c = (Component) e.getSource();
+					JPopupMenu popup = (JPopupMenu) c.getParent();
+					table = (JTable) popup.getInvoker();
 					int column = 0;
 					int row = table.getSelectedRow();
 					int value = Integer.parseInt((table.getModel().getValueAt(row, column)).toString());
-					//////////
-//		            System.out.println("VIEWCONTENT");
-//		          //  System.out.println(table.getSelectedRow() + " : " + table.getSelectedColumn());
-//		            System.out.println(value);
-//		            
-//		            //////////////////
-		            ArrayList<Content> content = new PlaylistController().getOneListContent(value);
+					ArrayList<Content> content = new PlaylistController().getOneListContent(value);
 					showOnePlaylist(modeloCanciones, content);
-					
+
 				} catch (NumberFormatException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				} catch (Exception e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-	            
-//	            System.out.println("VIEWCONTENT");
-//	            System.out.println(table.getSelectedRow() + " : " + table.getSelectedColumn());
-	        }
-      });
-        
-        popup.add( menuViewContent );
-		
-        
-        table.addMouseListener( new MouseAdapter()
-        {
-            public void mousePressed(MouseEvent e)
-            {
-                //System.out.println("pressed");
-            }
 
-            public void mouseReleased(MouseEvent e)
-            {
-                if (e.isPopupTrigger())
-                {
-                    JTable source = (JTable)e.getSource();
-                    int row = source.rowAtPoint( e.getPoint() );
-                    int column = source.columnAtPoint( e.getPoint() );
+			}
+		});
 
-                    if (! source.isRowSelected(row))
-                        source.changeSelection(row, column, false, false);
+		popup.add(menuViewContent);
 
-                    popup.show(e.getComponent(), e.getX(), e.getY());
-                 //   System.out.println(" On release X"+e.getX()+ "Y"+e.getY());
+		table.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+			}
 
-                }
-            }
-        });
-        table.setPreferredScrollableViewportSize(table.getPreferredSize());
-        
-        
-      
-		
-		/////////////////////////////////
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					JTable source = (JTable) e.getSource();
+					int row = source.rowAtPoint(e.getPoint());
+					int column = source.columnAtPoint(e.getPoint());
+
+					if (!source.isRowSelected(row))
+						source.changeSelection(row, column, false, false);
+
+					popup.show(e.getComponent(), e.getX(), e.getY());
+
+				}
+			}
+		});
+		table.setPreferredScrollableViewportSize(table.getPreferredSize());
+
 		JButton btnseeAllPlaylists = new JButton("See Playlists");
 		btnseeAllPlaylists.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 
-					ArrayList<Playlist> playlists = new PlaylistController().getAllPlaylistsFromAccount(Session.getInstance().getAccount().getId()  );
-					
-					 showPlaylist(modelo, playlists);
-					
+					ArrayList<Playlist> playlists = new PlaylistController()
+							.getAllPlaylistsFromAccount(Session.getInstance().getAccount().getId());
+
+					showPlaylist(modelo, playlists);
+
 					new PlaylistController().getAllPlaylistsFromAccount(2);
 				} catch (SQLException e1) {
-					// TODO ErrorPOP UP
-					//e1.printStackTrace();
 				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					//e1.printStackTrace();
 				}
-				
+
 			}
 		});
 		btnseeAllPlaylists.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnseeAllPlaylists.setBounds(750, 389, 131, 58);
 		add(btnseeAllPlaylists);
-		
+
 		JButton btnExport = new JButton("Export Playlists");
 		btnExport.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					ArrayList<Playlist> playlists = new PlaylistController().getAllPlaylistsFromAccount(Session.getInstance().getAccount().getId() );
-					new FileManager().writeObject(playlists, String.valueOf(Session.getInstance().getAccount().getId()));
-					
+					ArrayList<Playlist> playlists = new PlaylistController()
+							.getAllPlaylistsFromAccount(Session.getInstance().getAccount().getId());
+					new FileManager().writeObject(playlists,
+							String.valueOf(Session.getInstance().getAccount().getId()));
+
 					new PlaylistController().getAllPlaylistsFromAccount(Session.getInstance().getAccount().getId());
 				} catch (SQLException e1) {
-					
-//					System.out.println("Export Failed");
-//					e1.printStackTrace();
+
 				} catch (Exception e1) {
-					
-//					System.out.println("Export Failed");
-//					e1.printStackTrace();
+
 				}
 			}
 		});
-		
+
 		btnExport.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnExport.setBounds(750, 477, 131, 58);
 		add(btnExport);
-		
+
 		JButton btnImport = new JButton("Import Playlists");
 		btnImport.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				////
-				////SAme thing, index in readboject gos for testing, a different one should be used
 				try {
 					@SuppressWarnings("unchecked")
-					ArrayList<Playlist> playlists  = (ArrayList<Playlist>)new FileManager().readObject(String.valueOf(Session.getInstance().getAccount().getId()));
-					showPlaylist( modelo,  playlists);
+					ArrayList<Playlist> playlists = (ArrayList<Playlist>) new FileManager()
+							.readObject(String.valueOf(Session.getInstance().getAccount().getId()));
+					showPlaylist(modelo, playlists);
 				} catch (Exception e1) {
-					System.out.println("Import Failed");				
+					System.out.println("Import Failed");
 					e1.printStackTrace();
 				}
-				
-				
-				
-				
+
 			}
 		});
 		btnImport.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnImport.setBounds(750, 546, 131, 58);
 		add(btnImport);
-		
+
 		JButton btnAddNewPlaylist = new JButton("Add new  Playist");
 		btnAddNewPlaylist.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					var title = javax.swing.JOptionPane.showInputDialog("Write a name to indenity the playlist?");
-					 if (title == null)
-						 return;
+					if (title == null)
+						return;
 					new PlayListManager().createPlaylist(Session.getInstance().getAccount().getId(), title.trim());
 				} catch (HeadlessException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				} catch (Exception e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
@@ -290,36 +237,27 @@ public class MisFavoritos extends JPanel {
 		btnAddNewPlaylist.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnAddNewPlaylist.setBounds(762, 312, 120, 50);
 		add(btnAddNewPlaylist);
-	
+
 	}
-	
-	public void showOnePlaylist(DefaultTableModel modeloCanciones, ArrayList<Content> playlist)
-	{
+
+	public void showOnePlaylist(DefaultTableModel modeloCanciones, ArrayList<Content> playlist) {
 		modeloCanciones.setRowCount(0);
 
-		for(Content content: playlist)
-		{
-		String[] linea = { content.getTitle(), "pending to convert date time" , String.valueOf(content.getReproductions()) };
-		modeloCanciones.addRow(linea);
+		for (Content content : playlist) {
+			String[] linea = { content.getTitle(), "pending to convert date time",
+					String.valueOf(content.getReproductions()) };
+			modeloCanciones.addRow(linea);
 		}
-	
-	
-	
-}
-	
-	
-		public void showPlaylist(DefaultTableModel modelo, ArrayList<Playlist> playlists)
-		{
-			modelo.setRowCount(0);
-			for(Playlist playlist: playlists)
-			{
-			String[] linea = { String.valueOf(playlist.getPlaylistId()), playlist.getTitle()};
-			modelo.addRow(linea);
-			}
-		
-		
-		
+
 	}
-		
-		
+
+	public void showPlaylist(DefaultTableModel modelo, ArrayList<Playlist> playlists) {
+		modelo.setRowCount(0);
+		for (Playlist playlist : playlists) {
+			String[] linea = { String.valueOf(playlist.getPlaylistId()), playlist.getTitle() };
+			modelo.addRow(linea);
+		}
+
+	}
+
 }
